f4ace7019d992f19bdfe8da7185f21c225603f19
achilles@souza <77950840+akillez01@users.noreply.github.com>
2024-12-26 15:57:05 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,15 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Diretório padrão de saída para o build (pode ser alterado, se necessário)
+  },
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // A URL do backend
-        changeOrigin: true, // Alterar a origem para corresponder ao backend
-        secure: false, // Caso o backend use HTTP em vez de HTTPS
+        target: "http://localhost:5000", // URL do backend para desenvolvimento
+        changeOrigin: true, // Altera a origem para corresponder ao backend
+        secure: false, // Define como false caso o backend use HTTP
       },
     },
   },
