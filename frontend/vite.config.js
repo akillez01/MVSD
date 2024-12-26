22f7bc54344374c5adcb2103026590cefe365d34
akillez01 <achilles.oliveira.souza@gmail.com>
2024-12-26 17:52:58 -0500

    Adiciona variável de ambiente VITE_BACKEND_URL e corrige configuração do Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,15 @@ import { defineConfig } from "vite";
 
 dotenv.config();
 
+console.log("VITE_BACKEND_URL:", process.env.VITE_BACKEND_URL);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000", // URL do backend para desenvolvimento
+        target: process.env.VITE_BACKEND_URL || "http://localhost:5000", // URL do backend para desenvolvimento
         changeOrigin: true, // Altera a origem para corresponder ao backend
         secure: false, // Define como false caso o backend use HTTP
       },
